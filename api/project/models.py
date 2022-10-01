37851aab19a65477b6fe249dd3e47ae44018eb63
Almaz Yarullin <almas2901@yandex.ru>
2022-10-01 21:39:32 +0300

    Add timestamps

--- api/project/models.py
+++ api/project/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=20, verbose_name='Name')
     description = models.TextField(null=True, blank=True, verbose_name='Description')
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_updated = models.DateTimeField(auto_now=True)
