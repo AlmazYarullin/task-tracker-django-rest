906fefafc7a9fd5134fc5248063df172a1446193
Almaz Yarullin <almas2901@yandex.ru>
2022-10-12 14:56:32 +0300

    Add user urls

--- task_tracker/urls.py
+++ task_tracker/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('project/', include('api.project.urls')),
-    path('user/', include('api.user.urls'))
+    path('user/', include('api.user.urls')),
 ]
